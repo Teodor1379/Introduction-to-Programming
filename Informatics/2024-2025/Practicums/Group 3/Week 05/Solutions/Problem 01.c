71acc6f21b9c8139fa73f73d8712e834e9f4b711
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-14 13:44:50 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 01.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+
+void solutionOne() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 0;
+
+    for (long long i = 1; i <= n; ++i) {
+        result = result + i;
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionTwo() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 0;
+
+    for (long long i = 1; i <= n; ++i) {
+        result = result + (i * i);
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionThree() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 0;
+
+    for (long long i = 1; i <= n; ++i) {
+        result = result + (i * i * i);
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionFour() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 0;
+
+    for (long long i = 1; i <= n; ++i) {
+        result = result + (i * (i + 1));
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionFive() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 0;
+
+    for (long long i = 2; i <= 2 * n; i = i + 2) {
+        result = result + ((i - 1) * i);
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionSix() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 0;
+
+    for (long long i = 1; i <= n; ++i) {
+        result = result + (i * (i + 1) * (i + 2));
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionSeven() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 1;
+
+    for (long long i = n; i >= 1; --i) {
+        result = result * i;
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionEight() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 1;
+
+    for (long long i = n; i >= 1; i = i - 2) {
+        result = result * i;
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+void solutionNine() {
+    long long n = 0;
+
+    do {
+        printf("Enter the number N: ");
+        scanf("%lld", &n);
+    } while (n < 0);
+
+    long long result = 1;
+
+    for (long long i = n; i >= 1; i = i - 3) {
+        result = result * i;
+    }
+
+    printf("The result is: %lld\n", result);
+}
+
+int main() {
+    // solutionOne();
+    // solutionTwo();
+    // solutionThree();
+    // solutionFour();
+    // solutionFive();
+    // solutionSix();
+    // solutionSeven();
+    // solutionEight();
+    // solutionNine();
+
+    return 0;
+}
