a0fd976781f12bb96f4e4a5f351e0fada0afe1ee
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-14 14:00:02 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 07.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int number = 0;
+
+    printf("Enter the number: ");
+    scanf("%u", &number);
+
+    unsigned int reversed   =   0       ;
+    unsigned int temporary  =   number  ;
+
+    while (temporary > 0) {
+        unsigned int digit = (temporary % 10);
+        reversed    = reversed * 10 + digit;
+        temporary   = temporary / 10;
+    }
+
+    if (number == reversed) {
+        puts("Valid");
+    } else {
+        puts("Invalid");
+    }
+
+    return 0;
+}
