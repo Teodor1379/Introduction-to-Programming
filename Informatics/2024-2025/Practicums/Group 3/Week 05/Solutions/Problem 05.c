8a5567718b86265992a2700a2bfdb5bb4ddf0d55
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-14 13:52:38 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 05.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int number = 0;
+
+    printf("Enter the number: ");
+    scanf("%u", &number);
+
+    if (number <= 1) {
+        printf("The number is not prime!\n");
+    } else {
+        int isPrime = 1;
+
+        for (unsigned int i = 2; i < number; ++i) {
+            if (number % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            printf("The number is prime!\n");
+        } else {
+            printf("The number is not prime!\n");
+        }
+    }
+
+    return 0;
+}
