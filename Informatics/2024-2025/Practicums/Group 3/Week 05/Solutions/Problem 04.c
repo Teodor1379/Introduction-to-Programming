eb8301d47a50bdbe35708a39e30068d501de26d9
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-14 13:51:13 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int    n   =   0;
+
+    printf("Enter the number N: ");
+    scanf("%u", &n);
+
+    unsigned int    sum     =   0;
+    unsigned int    product =   1;
+
+    for (unsigned int i = 1; i <= n; ++i) {
+        if (n % i == 0) {
+            sum     =   sum     + i;
+            product =   product * i;
+        }
+    }
+
+    printf("The sum     of the divisors of N is: %u\n", sum     );
+    printf("The product of the divisors of N is: %u\n", product );
+
+    return 0;
+}
