d777c5fdc7d51b10451cc02d2c4641510e44575b
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-24 10:53:25 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 03.c
@@ -0,0 +1,56 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#define MAX_CAPACITY 100
+
+void inputArray(        unsigned int array[], size_t size);
+void printArray(const   unsigned int array[], size_t size);
+
+void squareArray(unsigned int array[], size_t size);
+
+int main() {
+    unsigned int array[MAX_CAPACITY] = { 0 };
+
+    size_t size = 0;
+
+    do {
+        printf("Enter the size of the array: ");
+        scanf("%zu", &size);
+
+        while (getchar() != '\n');
+    } while (size == 0 || size > 100);
+
+    inputArray(array, size);
+    printArray(array, size);
+
+    squareArray(array, size);
+    printArray(array, size);
+
+    return 0;
+}
+
+void inputArray(unsigned int array[], size_t size) {
+    printf("Enter the elements of the array: ");
+
+    for (size_t i = 0; i < size; ++i) {
+        scanf("%u", &array[i]);
+    }
+}
+
+void printArray(const unsigned int array[], size_t size) {
+    printf("The elements of the array are: ");
+
+    for (size_t i = 0; i < size; ++i) {
+        printf("%u ", array[i]);
+    }
+
+    putchar('\n');
+}
+
+void squareArray(unsigned int array[], size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        array[i] = array[i] * array[i];
+    }
+}
