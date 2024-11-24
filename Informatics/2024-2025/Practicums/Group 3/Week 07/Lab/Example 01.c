e17d7793afdec828fa585986d14b33d3c25d03ef
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-24 10:54:24 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int array[4];
+
+    array[0] = 1;
+    array[1] = 3;
+    array[2] = 7;
+    array[3] = 9;
+
+    printf("%d\n", array[0]);
+    printf("%d\n", array[1]);
+    printf("%d\n", array[2]);
+    printf("%d\n", array[3]);
+
+    return 0;
+}
---
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 02.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    int array1[9] = { 1, 2, 3 };
+    int array2[]  = { 1, 2, 3 };
+
+    for (size_t i = 0; i < 9; ++i) {
+        printf("%d ", array1[i]);
+    }
+
+    putchar('\n');
+
+    for (size_t i = 0; i < 3; ++i) {
+        printf("%d ", array2[i]);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
