1a26b393101b2d3ecee0fe179aaa1cf40156c04c
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-24 10:14:59 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 08.c
@@ -0,0 +1,83 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#define MAX_CAPACITY 100
+
+void inputArray(        unsigned int array[], size_t size);
+void printArray(const   unsigned int array[], size_t size);
+
+void findNumberTimes(const unsigned int array[], size_t size, unsigned int number);
+
+int main() {
+    unsigned int array[MAX_CAPACITY] = { 0 };
+
+    size_t size = 0;
+
+    do {
+        printf("Enter the size of the array: ");
+        scanf("%zu", &size);
+
+        while (getchar() != '\n');
+    } while (size == 0 || size > 100);
+
+    unsigned int number = 0;
+
+    printf("Enter the number: ");
+    scanf("%u", &number);
+
+    inputArray(array, size);
+    printArray(array, size);
+
+    findNumberTimes(array, size, number);
+
+    return 0;
+}
+
+void inputArray(unsigned int array[], size_t size) {
+    printf("Enter the elements of the array: ");
+
+    for (size_t i = 0; i < size; ++i) {
+        scanf("%u", &array[i]);
+    }
+}
+
+void printArray(const unsigned int array[], size_t size) {
+    printf("The elements of the array are: ");
+
+    for (size_t i = 0; i < size; ++i) {
+        printf("%u ", array[i]);
+    }
+
+    putchar('\n');
+}
+
+void findNumberTimes(const unsigned int array[], size_t size, unsigned int number) {
+    size_t index1 = -1;
+    size_t index2 = -1;
+
+    unsigned int timesN = 0;
+
+    for (size_t i = 0; i < size; ++i) {
+        if (array[i] == number) {
+            timesN = timesN + 1;
+            
+            if (index1 == (size_t)(-1)) {
+                index1 = i;
+                index2 = i;
+            } else {
+                index2 = i;
+            }
+        }
+    }
+
+    if (timesN == 0) {
+        puts("The number was not found in the array!");
+    } else {
+        printf("The found was found %u times!\n", timesN);
+
+        printf("The start position of the found is: %zu\n", index1);
+        printf("The final position of the found is: %zu\n", index2);
+    }
+}
