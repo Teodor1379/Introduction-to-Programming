bb89446b18b1aaf9ff74b6fd04c9b5d105bc7cd2
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-24 11:07:37 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 06

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+unsigned int fibonacci(unsigned int n) {
+    if (n < 2) {
+        return n;
+    }
+
+    unsigned int number1    =   0;
+    unsigned int number2    =   1;
+    unsigned int number3    =   0;
+    unsigned int counter    =   2;
+
+    while (counter <= n) {
+        number3 = number2;
+        number2 = number1 + number2;
+        number1 = number3;
+
+        counter = counter + 1;
+    }
+
+    return number2;
+}
+
+int main() {
+    unsigned int n = 0;
+
+    printf("Enter the number: ");
+    scanf("%u", &n);
+
+    printf("The number of Fibonacci is: %u\n", fibonacci(n));
+
+    return 0;
+}
