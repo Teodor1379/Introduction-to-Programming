c97ed93951f43b948f87c97be4dce85ff3f9193e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-11-24 10:30:58 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 09.c
@@ -0,0 +1,121 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#define MAX_CAPACITY 100
+
+void inputArray(        unsigned int array[], size_t size);
+void printArray(const   unsigned int array[], size_t size);
+
+void findSubArrayMin(const unsigned int array[], size_t size);
+void findSubArrayMax(const unsigned int array[], size_t size);
+
+int main() {
+    unsigned int array[MAX_CAPACITY] = { 0 };
+
+    size_t size = 0;
+
+    do {
+        printf("Enter the size of the array: ");
+        scanf("%zu", &size);
+
+        while (getchar() != '\n');
+    } while (size == 0 || size > 100);
+
+    inputArray(array, size);
+    printArray(array, size);
+
+    findSubArrayMin(array, size);
+    findSubArrayMax(array, size);
+
+    return 0;
+}
+
+void inputArray(unsigned int array[], size_t size) {
+    printf("Enter the elements of the array: ");
+
+    for (size_t i = 0; i < size; ++i) {
+        scanf("%u", &array[i]);
+    }
+}
+
+void printArray(const unsigned int array[], size_t size) {
+    printf("The elements of the array are: ");
+
+    for (size_t i = 0; i < size; ++i) {
+        printf("%u ", array[i]);
+    }
+
+    putchar('\n');
+}
+
+void findSubArrayMin(const unsigned int array[], size_t size) {
+    size_t maxStart = 0;
+    size_t maxFinal = 0;
+
+    size_t maxLength = 0;
+    size_t curLength = 0;
+
+    size_t current = 0;
+
+    for (size_t i = 1; i < size; ++i) {
+        if (array[i - 1] >= array[i]) {
+            curLength = curLength + 1;
+        } else {
+            if (curLength > maxLength) {
+                maxLength   =   curLength;
+
+                maxStart    =   current;
+                maxFinal    =   i - 1;
+            }
+
+            current     =   i;
+            curLength   =   1;
+        }
+    }
+
+    if (curLength > maxLength) {
+        maxLength   =   curLength;
+
+        maxStart    =   current;
+        maxFinal    =   size - 1;
+    }
+
+    printArray(array + maxStart, maxFinal - maxStart + 1);
+}
+
+void findSubArrayMax(const unsigned int array[], size_t size) {
+    size_t maxStart = 0;
+    size_t maxFinal = 0;
+
+    size_t maxLength = 0;
+    size_t curLength = 0;
+
+    size_t current = 0;
+
+    for (size_t i = 1; i < size; ++i) {
+        if (array[i - 1] <= array[i]) {
+            curLength = curLength + 1;
+        } else {
+            if (curLength > maxLength) {
+                maxLength   =   curLength;
+
+                maxStart    =   current;
+                maxFinal    =   i - 1;
+            }
+
+            current     =   i;
+            curLength   =   1;
+        }
+    }
+
+    if (curLength > maxLength) {
+        maxLength   =   curLength;
+
+        maxStart    =   current;
+        maxFinal    =   size - 1;
+    }
+
+    printArray(array + maxStart, maxFinal - maxStart + 1);
+}
