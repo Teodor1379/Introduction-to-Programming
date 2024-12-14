2ab165d1114ac2e55195bac4c62e9f0a79a52f04
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-14 23:28:07 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 02

--- Informatics/2024-2025/Practicums/Group 3/Week 02/Solutions/Problem 01.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 02/Solutions/Problem 01.c
@@ -3,11 +3,11 @@
 
 int main() {
     int     a   = 9     ;
-    double  b   = 1.37  ;
+    float   b   = 1.37  ;
     char    c   = 'J'   ;
 
     printf("%d\n",  a);
-    printf("%lf\n", b);
+    printf("%f\n",  b);
     printf("%c\n",  c);
 
     return 0;
