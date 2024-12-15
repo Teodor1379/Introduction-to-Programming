751251a7d128af5f3fa3733b2ef7fd5dbeb65ea1
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-15 13:46:16 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 02

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 02/Lab/Example 05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+#define MILES_INTO_KILOMETERS 1.609344
+
+
+
+int main() {
+    float velocityMPH   =   0.0;
+    float velocityKMPH  =   0.0;
+
+    printf("Enter the Velocity in Miles per Hour: ");
+
+    scanf("%f", &velocityMPH);
+
+    velocityKMPH = velocityMPH * MILES_INTO_KILOMETERS;
+
+    printf("The Velocity in Kilometers per Hour is: %f\n", velocityKMPH);
+
+    return 0;
+}
