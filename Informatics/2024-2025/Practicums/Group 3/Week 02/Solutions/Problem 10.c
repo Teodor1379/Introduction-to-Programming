39bea0529ace973d72800ae351af7c3c2913ef04
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-23 12:45:14 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 02

--- Informatics/2024-2025/Practicums/Group 3/Week 02/Solutions/Problem 10.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 02/Solutions/Problem 10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 
+
 int main() {
     int year = 0;
 
@@ -8,10 +9,12 @@ int main() {
 
     scanf("%d", &year);
 
+
     int condition1 = ((year % 4 == 0) && (year % 100 != 0));
     int condition2 = (year % 400 == 0);
 
     printf("Leap Year: %d\n", condition1 || condition2);
+
 
     return 0;
 }
