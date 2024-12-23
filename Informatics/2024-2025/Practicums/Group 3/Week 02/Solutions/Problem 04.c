15d94cf7353684c00c603ce8ecafb412872ece6c
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-23 11:02:49 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 02

--- Informatics/2024-2025/Practicums/Group 3/Week 02/Solutions/Problem 04.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 02/Solutions/Problem 04.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 
 
+
 int main() {
     int x = 0;
     int y = 0;
 
-    printf("Enter x: ");    scanf("%d", &x);
-    printf("Enter y: ");    scanf("%d", &y);
 
-    printf("x + y: %d\n", x + y);
-    printf("x - y: %d\n", x - y);
-    printf("x * y: %d\n", x * y);
-    printf("x / y: %d\n", x / y);
-
-    printf("x %% y: %d\n", x % y);
+    printf("Enter the number X: "); scanf("%d", &x);
+    printf("Enter the number Y: "); scanf("%d", &y);
 
 
-    printf("x < y: %d\n", x < y);
-    printf("x > y: %d\n", x > y);
+    printf("X + Y: %d\n", x + y);
+    printf("X - Y: %d\n", x - y);
+    printf("X * Y: %d\n", x * y);
+    printf("X / Y: %d\n", x / y);
 
-    printf("x <= y: %d\n", x <= y);
-    printf("x >= y: %d\n", x >= y);
+    printf("X %% Y: %d\n", x % y);
 
-    printf("x == y: %d\n", x == y);
-    printf("x != y: %d\n", x != y);
+
+    printf("X < Y: %d\n", x < y);
+    printf("X > Y: %d\n", x > y);
+
+    printf("X <= Y: %d\n", x <= y);
+    printf("X >= Y: %d\n", x >= y);
+
+    printf("X == Y: %d\n", x == y);
+    printf("X != Y: %d\n", x != y);
 
     
-    printf("x && y: %d\n", x && y);
-    printf("x || y: %d\n", x || y);
+    printf("X && Y: %d\n", x && y);
+    printf("X || Y: %d\n", x || y);
+
 
     return 0;
 }
