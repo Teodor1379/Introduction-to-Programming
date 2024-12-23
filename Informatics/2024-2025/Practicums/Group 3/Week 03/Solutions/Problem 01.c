8e4be85f953a25fb998538e410559394118f3b02
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-23 13:36:31 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 03

--- Informatics/2024-2025/Practicums/Group 3/Week 03/Solutions/Problem 01.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 03/Solutions/Problem 01.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 int main() {
     unsigned int x  =   0;
     unsigned int y  =   0;
@@ -7,12 +9,14 @@ int main() {
     printf("Enter the number X: "); scanf("%u", &x);
     printf("Enter the number Y: "); scanf("%u", &y);
 
+
     printf("The bitwise NOT of X is: %u\n", ~x);
     printf("The bitwise NOT of Y is: %u\n", ~y);
 
     printf("The bitwise AND of X and Y is: %u\n",   x & y);
     printf("The bitwise OR  of X and Y is: %u\n",   x | y);
     printf("The bitwise XOR of X and Y is: %u\n",   x ^ y);
+
 
     return 0;
 }
