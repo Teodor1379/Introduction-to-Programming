6c0857f6716c9054ba711a66279cd7bf032e00b1
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-23 13:56:44 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 02

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 02/Lab/Example 09.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+
+
+
+void numbersIntegers    ();
+void numbersFloats      ();
+void numbersDoubles     ();
+void numbersCharacters  ();
+
+
+
+void clearStandardInput();
+
+
+
+int main() {
+    // Logical Operators
+
+    numbersIntegers     ();
+    numbersFloats       ();
+    numbersDoubles      ();
+    numbersCharacters   ();
+
+    return 0;
+}
+
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+void numbersIntegers() {
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+
+
+    int number1 = 0;
+    int number2 = 0;
+
+    printf("Enter the number 1: "); scanf("%d", &number1);
+    printf("Enter the number 2: "); scanf("%d", &number2);
+
+
+    printf("! Number1: %d\n",   !number1);
+    printf("! Number2: %d\n",   !number2);
+
+
+    printf("Number1 && Number2: %d\n",  number1 && number2);
+    printf("Number1 || Number2: %d\n",  number1 || number2);
+
+
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+}
+
+void numbersFloats() {
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+
+
+    float number1   =   0.0;
+    float number2   =   0.0;
+
+    printf("Enter the number 1: "); scanf("%f", &number1);
+    printf("Enter the number 2: "); scanf("%f", &number2);
+
+
+    printf("! Number1: %d\n",   !number1);
+    printf("! Number2: %d\n",   !number2);
+
+
+    printf("Number1 && Number2: %d\n",  number1 && number2);
+    printf("Number1 || Number2: %d\n",  number1 || number2);
+
+
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+}
+
+void numbersDoubles() {
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+
+
+    double number1  =   0.0;
+    double number2  =   0.0;
+
+    printf("Enter the number 1: "); scanf("%lf",    &number1);
+    printf("Enter the number 2: "); scanf("%lf",    &number2);
+
+
+    printf("! Number1: %d\n",   !number1);
+    printf("! Number2: %d\n",   !number2);
+
+
+    printf("Number1 && Number2: %d\n", number1 && number2);
+    printf("Number1 || Number2: %d\n", number1 || number2);
+
+
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+}
+
+void numbersCharacters() {
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+
+
+    char number1    =   'T';
+    char number2    =   'J';
+
+    clearStandardInput();   printf("Enter the Number 1: "); scanf("%c", &number1);
+    clearStandardInput();   printf("Enter the Number 2: "); scanf("%c", &number2);
+
+
+    printf("! Number1: %c\n",   !number1);
+    printf("! Number2: %c\n",   !number2);
+
+
+    printf("Number1 && Number2: %c\n",  number1 && number2);
+    printf("Number1 || Number2: %c\n",  number1 || number2);
+
+
+    putchar('\n');
+    putchar('\n');
+    putchar('\n');
+}
