94c987040298b331445eabb75a79948c756fe706
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-24 02:50:25 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 03

--- Informatics/2024-2025/Practicums/Group 3/Week 03/Solutions/Problem 02.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 03/Solutions/Problem 02.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+
+
 int main() {
     unsigned int z  =   0;
     unsigned int n  =   0;
 
-    printf("Enter the number Z: "); scanf("%u", &z);
-    printf("Enter the number N: "); scanf("%u", &n);
+    printf("Enter the number Z: "); if (scanf("%u", &z) != 1)   { puts("Input Error!"); return 1;   }
+    printf("Enter the number N: "); if (scanf("%u", &n) != 1)   { puts("Input Error!"); return 1;   }
 
     if (n >= 8 * sizeof(unsigned int)) {
         printf("Invalid value for N!\n");
@@ -13,8 +15,10 @@ int main() {
         return 1;
     }
 
+
     printf("The bitwise left    shift of Z with N is: %u\n",    z << n);
     printf("The bitwise right   shift of Z with N is: %u\n",    z >> n);
+
 
     return 0;
 }
