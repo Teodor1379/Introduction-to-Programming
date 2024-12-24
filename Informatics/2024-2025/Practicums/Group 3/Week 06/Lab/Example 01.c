82e755b976b492f9e3f649bca5898934a7c65258
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-24 05:45:54 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 06

--- Informatics/2024-2025/Practicums/Group 3/Week 06/Lab/Example 01.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 06/Lab/Example 01.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+
+
 int factorial(int number);
+
+
 
 int min2(int number1, int number2);
 int min3(int number1, int number2, int number3);
@@ -8,8 +12,11 @@ int min3(int number1, int number2, int number3);
 int max2(int number1, int number2);
 int max3(int number1, int number2, int number3);
 
+
+
 int main() {
     printf("The factorial of 9 is: %d\n",   factorial(9));
+
 
     printf("The min of 2 arguments is: %d\n",   min2(1, 3)      );
     printf("The min of 3 arguments is: %d\n",   min3(1, 3, 7)   );
@@ -17,8 +24,11 @@ int main() {
     printf("The max of 2 arguments is: %d\n",   max2(1, 3)      );
     printf("The max of 3 arguments is: %d\n",   max3(1, 3, 7)   );
 
+
     return 0;
 }
+
+
 
 int factorial(int number) {
     int result = 1;
@@ -29,6 +39,8 @@ int factorial(int number) {
 
     return result;
 }
+
+
 
 int min2(int number1, int number2)              {   return number1 < number2 ? number1 : number2;   }
 int min3(int number1, int number2, int number3) {   return min2(number1, min2(number2, number3));   }
