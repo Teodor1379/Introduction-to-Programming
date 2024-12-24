e918219734e606e50abc13762563d6e4f4e39a8c
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-24 14:38:16 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 04

--- Informatics/2024-2025/Practicums/Group 3/Week 04/Solutions/Problem 01.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 04/Solutions/Problem 01.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+
+
 #define SIZE 32
+
+
 
 void printNumberBinary(unsigned int number) {
     unsigned int array[SIZE] = { 0 };
@@ -21,12 +25,14 @@ void printNumberBinary(unsigned int number) {
     putchar('\n');
 }
 
+
+
 int main() {
     unsigned int n  =   0;
     unsigned int s  =   0;
 
-    printf("Enter the number N: "); scanf("%u", &n);
-    printf("Enter the number S: "); scanf("%u", &s);
+    printf("Enter the number N: "); if (scanf("%u", &n) != 1)   { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number S: "); if (scanf("%u", &s) != 1)   { puts("Invalid Input!");   return 1;   }
 
     if (s >= 8 * sizeof(unsigned int)) {
         printf("Invalid value for S!\n");
@@ -34,11 +40,13 @@ int main() {
         return 1;
     }
 
+
     unsigned int bit = (n >> s) & 1;
 
     printNumberBinary(n);
 
     printf("The S-th bit of number N is: %u\n", bit);
+
 
     return 0;
 }
