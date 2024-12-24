133f987b506dd51fe82d8816b656a35f2d732808
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-24 20:34:56 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 04

--- Informatics/2024-2025/Practicums/Group 3/Week 04/Solutions/Problem 02.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 04/Solutions/Problem 02.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+
+
 #define SIZE 32
+
+
 
 void printNumberBinary(unsigned int number) {
     unsigned int array[SIZE] = { 0 };
@@ -21,37 +25,43 @@ void printNumberBinary(unsigned int number) {
     putchar('\n');
 }
 
+
+
 int main() {
     unsigned int n = 0;
     unsigned int s = 0;
     unsigned int r = 0;
 
-    printf("Enter the number N: "); scanf("%u", &n);
-    printf("Enter the number S: "); scanf("%u", &s);
-    printf("Enter the number R: "); scanf("%u", &r);
+    printf("Enter the number N: "); if (scanf("%u", &n) != 1)   { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number S: "); if (scanf("%u", &s) != 1)   { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number R: "); if (scanf("%u", &r) != 1)   { puts("Invalid Input!");   return 1;   }
 
     if (s >= 8 * sizeof(unsigned int)) {
         printf("Invalid value for S!\n");
 
-        return 1;
+        return 2;
     }
 
     if (r >= 8 * sizeof(unsigned int)) {
         printf("Invalid value for R!\n");
 
-        return 1;
+        return 2;
     }
 
+
     printf("The old version of the number is: ");
+
     printNumberBinary(n);
 
     n = (n & ~(1 << s));
     n = (n |  (1 << r));
 
     printf("The new version of the number is: ");
+
     printNumberBinary(n);
 
-    printf("The new number N is: %u\n", n);
+    printf("The number itself is: %u\n", n);
+
 
     return 0;
 }
