d8ef0b6e7f249a2317e7dd0d13a238948e2c16d4
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 22:52:32 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

--- Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 02.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 02.c
@@ -1,23 +1,24 @@
-#include <stddef.h>
-#include <stdlib.h>
-
 #include <stdio.h>
+
+
 
 int main() {
     int array1[9] = { 1, 2, 3 };
     int array2[]  = { 1, 2, 3 };
 
-    for (size_t i = 0; i < 9; ++i) {
+
+    for (unsigned int i = 0; i < 9; ++i) {
         printf("%d ", array1[i]);
     }
 
     putchar('\n');
 
-    for (size_t i = 0; i < 3; ++i) {
+    for (unsigned int i = 0; i < 3; ++i) {
         printf("%d ", array2[i]);
     }
 
     putchar('\n');
+
 
     return 0;
 }
