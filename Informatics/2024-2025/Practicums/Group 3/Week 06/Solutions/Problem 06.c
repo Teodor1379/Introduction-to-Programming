e7c4dbb31d24a8c1a1b428c8a7434970d116e51d
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 18:07:09 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 06

--- Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 06.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 06.c
@@ -43,5 +43,6 @@ int main() {
 
     showProgressionInformation(a, d, n);
 
+
     return 0;
 }
--- Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 07.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 07.c
@@ -2,6 +2,12 @@
 
 #include <stdio.h>
 
+
+
+#define EPSILON 1e-9
+
+
+
 double findSumProgression(double a, double q, unsigned int n) {
     return a * (1 - pow(q, n)) / (1 - q);
 }
@@ -18,24 +24,38 @@ void showProgressionInformation(double a, double q, unsigned int n) {
     printf("\nThe sum of the progression is: %lf\n", findSumProgression(a, q, n));
 }
 
+
+
 int main() {
     double  a   =   0.0;
     double  q   =   0.0;
 
-    printf("Enter the number A: "); scanf("%lf", &a);
-    printf("Enter the number Q: "); scanf("%lf", &q);
+    printf("Enter the number A: "); if (scanf("%lf", &a) != 1)  { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number Q: "); if (scanf("%lf", &q) != 1)  { puts("Invalid Input!");   return 1;   }
+
+
+    if (fabs(q) < EPSILON) {
+        printf("Invalid value for Q!\n");
+
+        return 1;
+    }
+
 
     unsigned int n  =   0;
 
     do {
         printf("Enter the number N: ");
 
-        scanf("%u", &n);
+        if (scanf("%u", &n) != 1) {
+            puts("Invalid Input!");
 
-        while (getchar() != '\n');
+            return 1;
+        }
     } while (n == 0);
 
+
     showProgressionInformation(a, q, n);
+
 
     return 0;
 }
