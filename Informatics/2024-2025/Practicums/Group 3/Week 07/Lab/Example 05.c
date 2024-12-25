3976b8aa185d71dd3d062785d0ea3c7000cc4c2f
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 23:16:53 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 05.c
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+#define MAX_CAPACITY 100
+
+
+
+void clearStandardInput();
+
+
+
+int             readElem();
+unsigned int    readSize();
+
+
+
+void inputArray(        int array[], unsigned int size);
+void printArray(const   int array[], unsigned int size);
+
+
+
+int containsOne(const   int array[], unsigned int size);
+
+
+
+int main() {
+    int array[MAX_CAPACITY] = { 0 };
+
+    unsigned int size = readSize();
+
+    inputArray(array, size);
+    printArray(array, size);
+
+    printf("Is there an odd number? %s\n", containsOne(array, size) ? "Yes" : "No");
+
+    return 0;
+}
+
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+int readElem() {
+    int element         =   0;
+    int inputArgument   =   0;
+
+    do {    
+        inputArgument = scanf("%d", &element);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return element;
+}
+
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the size of the array: ");
+
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > MAX_CAPACITY);
+
+    return size;
+}
+
+
+
+void inputArray(int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
+        printf("Invalid size for the array!\n");
+
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter the elements of the array: ");
+
+    for (unsigned int i = 0; i < size; ++i) {
+        array[i] = readElem();
+    }
+}
+
+void printArray(const int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
+        printf("Invalid size for the array!\n");
+
+        exit(EXIT_FAILURE);
+    }
+
+    printf("The elements of the array are: ");
+
+    for (unsigned int i = 0; i < size; ++i) {
+        printf("%d ", array[i]);
+    }
+
+    putchar('\n');
+}
+
+
+int containsOne(const int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
+        printf("Invalid size for the array!\n");
+
+        exit(EXIT_FAILURE);
+    }
+
+    for (unsigned int i = 0; i < size; ++i) {
+        if (array[i] % 2 == 1) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
