ad128c0a7752ad752df2e1940980031cd2d0099b
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 22:58:44 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

--- Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 03.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 03.c
@@ -1,27 +1,35 @@
-#include <stddef.h>
 #include <stdlib.h>
 
 #include <stdio.h>
 
+
+
 #define MAX_CAPACITY 100
 
-void inputArray(        unsigned int array[], size_t size);
-void printArray(const   unsigned int array[], size_t size);
 
-void squareArray(unsigned int array[], size_t size);
+
+void clearStandardInput();
+
+
+
+unsigned int    readElem();
+unsigned int    readSize();
+
+
+
+void inputArray(        unsigned int array[], unsigned int size);
+void printArray(const   unsigned int array[], unsigned int size);
+
+
+
+void squareArray(unsigned int array[], unsigned int size);
+
+
 
 int main() {
     unsigned int array[MAX_CAPACITY] = { 0 };
 
-    size_t size = 0;
-
-    do {
-        printf("Enter the size of the array: ");
-
-        scanf("%zu", &size);
-
-        while (getchar() != '\n');
-    } while (size == 0 || size > 100);
+    unsigned int size = readSize();
 
     inputArray(array, size);
     printArray(array, size);
@@ -32,26 +40,90 @@ int main() {
     return 0;
 }
 
-void inputArray(unsigned int array[], size_t size) {
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+unsigned int readElem() {
+    unsigned int elem   =   0;
+    int inputArgument   =   0;
+
+    do {
+        inputArgument = scanf("%u", &elem);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return elem;
+}
+
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the size of the array: ");
+
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > MAX_CAPACITY);
+
+    return size;
+}
+
+
+
+void inputArray(unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
+        printf("Invalid size for the array!\n");
+
+        exit(EXIT_FAILURE);
+    }
+
     printf("Enter the elements of the array: ");
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         scanf("%u", &array[i]);
     }
 }
 
-void printArray(const unsigned int array[], size_t size) {
+void printArray(const unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
+        printf("Invalid size for the array!\n");
+
+        exit(EXIT_FAILURE);
+    }
+
     printf("The elements of the array are: ");
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         printf("%u ", array[i]);
     }
 
     putchar('\n');
 }
 
-void squareArray(unsigned int array[], size_t size) {
-    for (size_t i = 0; i < size; ++i) {
+
+
+void squareArray(unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
+        printf("Invalid size for the array!\n");
+
+        exit(EXIT_FAILURE);
+    }
+
+    for (unsigned int i = 0; i < size; ++i) {
         array[i] = array[i] * array[i];
     }
 }
