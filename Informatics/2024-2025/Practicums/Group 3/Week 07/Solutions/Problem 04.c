8ed1a762a136c31da8220c1695b019c0247cf086
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 17:53:13 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

--- Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 04.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 04.c
@@ -1,28 +1,36 @@
-#include <stddef.h>
 #include <stdlib.h>
 
 #include <stdio.h>
 
+
+
 #define MAX_CAPACITY 100
 
-void inputArray(        unsigned int array[], size_t size);
-void printArray(const   unsigned int array[], size_t size);
 
-void reverseArraySolution1(unsigned int array[], size_t size);
-void reverseArraySolution2(unsigned int array[], size_t size);
+
+void clearStandardInput();
+
+
+
+unsigned int    readElem();
+unsigned int    readSize();
+
+
+
+void inputArray(        unsigned int array[], unsigned int size);
+void printArray(const   unsigned int array[], unsigned int size);
+
+
+
+void reverseArraySolution1(unsigned int array[], unsigned int size);
+void reverseArraySolution2(unsigned int array[], unsigned int size);
+
+
 
 int main() {
     unsigned int array[MAX_CAPACITY] = { 0 };
 
-    size_t size = 0;
-
-    do {
-        printf("Enter the size of the array: ");
-
-        scanf("%zu", &size);
-
-        while (getchar() != '\n');
-    } while (size == 0 || size > 100);
+    unsigned int size = readSize();
 
     inputArray(array, size);
     printArray(array, size);
@@ -33,62 +41,107 @@ int main() {
     return 0;
 }
 
-void inputArray(unsigned int array[], size_t size) {
-    if (size > MAX_CAPACITY) {
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+unsigned int readElem() {
+    unsigned int elem   =   0;
+    int inputArgument   =   0;
+
+    do {
+        inputArgument = scanf("%u", &elem);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return elem;
+}
+
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the size of the array: ");
+
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > MAX_CAPACITY);
+
+    return size;
+}
+
+
+
+void inputArray(unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
 
     printf("Enter the elements of the array: ");
 
-    for (size_t i = 0; i < size; ++i) {
-        scanf("%u", &array[i]);
+    for (unsigned int i = 0; i < size; ++i) {
+        array[i] = readElem();
     }
 }
 
-void printArray(const unsigned int array[], size_t size) {
-    if (size > MAX_CAPACITY) {
+void printArray(const unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
 
     printf("The elements of the array are: ");
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         printf("%u ", array[i]);
     }
 
     putchar('\n');
 }
 
-void reverseArraySolution1(unsigned int array[], size_t size) {
-    if (size > MAX_CAPACITY) {
+
+void reverseArraySolution1(unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
 
     unsigned int temporary[MAX_CAPACITY] = { 0 };
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         temporary[size - i - 1] = array[i];
     }
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         array[i] = temporary[i];
     }
 }
 
-void reverseArraySolution2(unsigned int array[], size_t size) {
-    if (size > MAX_CAPACITY) {
+void reverseArraySolution2(unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
     
-    for (size_t i = 0; i < size / 2; ++i) {
+    for (unsigned int i = 0; i < size / 2; ++i) {
         unsigned int temporary = array[i];
 
         array[i] = array[size - i - 1];
