609b666f88351f5756408129fc7d9afff9f30945
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 14:58:16 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

--- Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 08.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 08.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+
+
 int main() {
     unsigned int    n   =   0;
     unsigned int    d   =   0;
     unsigned int    t   =   0;
 
-    printf("Enter the number N: "); scanf("%u", &n);
-    printf("Enter the number D: "); scanf("%u", &d);
+    printf("Enter the number N: "); if (scanf("%u", &n) != 1)   { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number D: "); if (scanf("%u", &d) != 1)   { puts("Invalid Input!");   return 1;   }
+
 
     while (n > 0) {
         unsigned int digit = n % 10;
@@ -15,6 +18,7 @@ int main() {
     }
 
     printf("The result is: %u\n", t);
+
 
     return 0;
 }
