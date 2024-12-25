f066bc8c64a8575a7839abf87583ba1a8d0df8e0
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 14:16:56 +0200

    Commit: Informatics - 2024-2035 - Practicums - Group 3 - Week 05

--- Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 02.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 02.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
+
+
 int main() {
     unsigned long long  n   =   0   ;
     double              x   =   0.0 ;
     double              r   =   1.0 ;
 
-    printf("Enter the number N: "); scanf("%llu", &n);
-    printf("Enter the number X: "); scanf("%lf",  &x);
+    printf("Enter the number N: "); if (scanf("%llu", &n) != 1) { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number X: "); if (scanf("%lf",  &x) != 1) { puts("Invalid Input!");   return 1;   }
+
 
     for (unsigned long long i = 1; i <= n; ++i) {
         r = r * x;
     }
 
     printf("The result is: %lf\n", r);
+
 
     return 0;
 }
