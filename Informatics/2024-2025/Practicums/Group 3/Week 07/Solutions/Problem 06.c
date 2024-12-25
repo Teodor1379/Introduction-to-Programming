274213c85c85aa875119764d02e221683bf90d54
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 22:49:25 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

--- Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 06.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 06.c
@@ -1,27 +1,35 @@
-#include <stddef.h>
 #include <stdlib.h>
 
 #include <stdio.h>
 
+
+
 #define MAX_CAPACITY 100
 
-void inputArray(        unsigned int array[], size_t size);
-void printArray(const   unsigned int array[], size_t size);
 
-void findMode(const unsigned int array[], size_t size);
+
+void clearStandardInput();
+
+
+
+unsigned int    readElem();
+unsigned int    readSize();
+
+
+
+void inputArray(        unsigned int array[], unsigned int size);
+void printArray(const   unsigned int array[], unsigned int size);
+
+
+
+void findMode(const unsigned int array[], unsigned int size);
+
+
 
 int main() {
     unsigned int array[MAX_CAPACITY] = { 0 };
 
-    size_t size = 0;
-
-    do {
-        printf("Enter the size of the array: ");
-
-        scanf("%zu", &size);
-
-        while (getchar() != '\n');
-    } while (size == 0 || size > 100);
+    unsigned int size = readSize();
 
     inputArray(array, size);
     printArray(array, size);
@@ -31,50 +39,97 @@ int main() {
     return 0;
 }
 
-void inputArray(unsigned int array[], size_t size) {
+
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+unsigned int readElem() {
+    unsigned int elem   =   0;
+    int inputArgument   =   0;
+
+    do {
+        inputArgument = scanf("%u", &elem);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return elem;
+}
+
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the size of the array: ");
+
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > MAX_CAPACITY);
+
+    return size;
+}
+
+
+
+void inputArray(unsigned int array[], unsigned int size) {
     if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
 
     printf("Enter the elements of the array: ");
 
-    for (size_t i = 0; i < size; ++i) {
-        scanf("%u", &array[i]);
+    for (unsigned int i = 0; i < size; ++i) {
+        array[i] = readElem();
     }
 }
 
-void printArray(const unsigned int array[], size_t size) {
+void printArray(const unsigned int array[], unsigned int size) {
     if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
     
     printf("The elements of the array are: ");
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         printf("%u ", array[i]);
     }
 
     putchar('\n');
 }
 
-void findMode(const unsigned int array[], size_t size) {
+
+
+void findMode(const unsigned int array[], unsigned int size) {
     if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
         
-        return;
+        exit(EXIT_FAILURE);
     }
 
     unsigned int maxCounter = 0;
     unsigned int indNumber  = 0;
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         unsigned int currentT = 1;
 
-        for (size_t j = i + 1; j < size; ++j) {
+        for (unsigned int j = i + 1; j < size; ++j) {
             if (array[j] == array[i]) {
                 currentT = currentT + 1;
             }
