af37da36b63124ad0bebcbff3cceeeeabcaebc3e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 18:03:57 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 06

--- Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 06.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 06/Solutions/Problem 06.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 double findSumProgression(double a, double d, unsigned int n) {
     return (2 * a + (n - 1) * d) * n / 2;
 }
@@ -16,22 +18,28 @@ void showProgressionInformation(double a, double d, unsigned int n) {
     printf("\nThe sum of the progression is: %lf\n", findSumProgression(a - n * d, d, n));
 }
 
+
+
 int main() {
     double  a   =   0.0;
     double  d   =   0.0;
 
-    printf("Enter the number A: "); scanf("%lf", &a);
-    printf("Enter the number D: "); scanf("%lf", &d);
+    printf("Enter the number A: "); if (scanf("%lf", &a) != 1)  { puts("Invalid Input!");   return 1;   }
+    printf("Enter the number D: "); if (scanf("%lf", &d) != 1)  { puts("Invalid Input!");   return 1;   }
+
 
     unsigned int n  =   0;
 
     do {
         printf("Enter the number N: ");
 
-        scanf("%u", &n);
+        if (scanf("%u", &n) != 1) {
+            puts("Invalid Input!");
 
-        while (getchar() != '\n');
+            return 1;
+        }
     } while (n == 0);
+
 
     showProgressionInformation(a, d, n);
 
