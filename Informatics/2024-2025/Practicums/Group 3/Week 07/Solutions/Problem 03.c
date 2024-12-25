955f94a39e7b92d2f3994097bc0b6c3d6c20f34d
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 16:55:09 +0200

    Commit: Inforatics - 2024-2025 - Practicums - Group 3 - Week 07

--- Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 03.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Solutions/Problem 03.c
@@ -1,36 +1,40 @@
-#include <stddef.h>
 #include <stdlib.h>
 
 #include <stdio.h>
 
+
+
 #define MAX_CAPACITY 100
 
-void inputArray(        unsigned int array[], size_t size);
-void printArray(const   unsigned int array[], size_t size);
 
-void rotateArray(unsigned int array[], size_t size, size_t offset);
+
+void clearStandardInput();
+
+
+
+unsigned int    readElem();
+unsigned int    readOffs();
+unsigned int    readSize();
+
+
+
+void inputArray(        unsigned int array[], unsigned int size);
+void printArray(const   unsigned int array[], unsigned int size);
+
+
+
+void rotateArray(unsigned int array[], unsigned int size, unsigned int offset);
+
+
 
 int main() {
     unsigned int array[MAX_CAPACITY] = { 100 };
 
-    size_t size = 0;
-    size_t offs = 0;
-
-    do {
-        printf("Enter the size of the array: ");
-
-        scanf("%zu", &size);
-
-        while (getchar() != '\n');
-    } while (size == 0 || size > 100);
+    unsigned int size = readSize();
+    unsigned int offs = readOffs();
 
     inputArray(array, size);
     printArray(array, size);
-
-    while (getchar() != '\n');
-
-    printf("Enter the offset of the array: ");
-    scanf("%zu", &offs);
 
     rotateArray(array, size, offs);
     printArray(array, size);
@@ -38,49 +42,111 @@ int main() {
     return 0;
 }
 
-void inputArray(unsigned int array[], size_t size) {
-    if (size > MAX_CAPACITY) {
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+unsigned int readElem() {
+    unsigned int elem   =   0;
+    int inputArgument   =   0;
+
+    do {
+        inputArgument = scanf("%u", &elem);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return elem;
+}
+
+unsigned int readOffs() {
+    unsigned int offs   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the offset of the array: ");
+
+        inputArgument = scanf("%u", &offs);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return offs;
+}
+
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the size of the array: ");
+
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > MAX_CAPACITY);
+
+    return size;
+}
+
+
+
+void inputArray(unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
 
     printf("Enter the elements of the array: ");
 
-    for (size_t i = 0; i < size; ++i) {
-        scanf("%u", &array[i]);
+    for (unsigned int i = 0; i < size; ++i) {
+        array[i] = readElem();
     }
 }
 
-void printArray(const unsigned int array[], size_t size) {
-    if (size > MAX_CAPACITY) {
+void printArray(const unsigned int array[], unsigned int size) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
-        return;
+        exit(EXIT_FAILURE);
     }
 
     printf("The elements of the array are: ");
 
-    for (size_t i = 0; i < size; ++i) {
+    for (unsigned int i = 0; i < size; ++i) {
         printf("%u ", array[i]);
     }
 
     putchar('\n');
 }
 
-void rotateArray(unsigned int array[], size_t size, size_t offset) {
-    if (size > MAX_CAPACITY) {
+
+
+void rotateArray(unsigned int array[], unsigned int size, unsigned int offset) {
+    if (size == 0 || size > MAX_CAPACITY) {
         printf("Invalid size for the array!\n");
 
         return;
     }
 
-    size_t current = 0;
+    unsigned int current = 0;
 
     while (current < (offset % size)) {
         unsigned int element = array[size - 1];
 
-        for (size_t i = size - 1; i > 0; --i) {
+        for (unsigned int i = size - 1; i > 0; --i) {
             array[i] = array[i - 1];
         }
 
