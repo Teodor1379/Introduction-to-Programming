5bf905011d23763fd1506dd2b77afa5748b52409
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 15:06:21 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

--- Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 10.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 10.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 int main() {
     unsigned int    size    =   0   ;
     char            symbol  =   '$' ;
@@ -7,14 +9,22 @@ int main() {
     do {
         printf("Enter the size: ");
 
-        scanf("%u", &size);
+        if (scanf("%u", &size) != 1) {
+            puts("Invalid Input!");
+
+            return 1;
+        }
     } while (size == 0);
 
     getchar();
 
     printf("Enter the symbol: ");
 
-    scanf("%c", &symbol);
+    if (scanf("%c", &symbol) != 1) {
+        puts("Invalid Input!");
+
+        return 1;
+    }
 
     for (unsigned int i = 1; i <= size; ++i) {
         for (unsigned int j = 1; j <= i; ++j) {
