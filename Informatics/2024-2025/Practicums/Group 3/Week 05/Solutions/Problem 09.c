039400e71a506f88c10dc0a5382435bd3dc765f7
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-25 15:04:52 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 05

--- Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 09.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 05/Solutions/Problem 09.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+
+
 int main() {
     unsigned int n = 0;
 
     do {
         printf("Enter the number N: ");
 
-        scanf("%u", &n);
+        if (scanf("%u", &n) != 1) {
+            puts("Invalid Input!");
+
+            return 1;
+        }
     } while (n == 0);
+
 
     for (unsigned int i = 1; i <= n; ++i) {
         for (unsigned int j = 1; j <= n; ++j) {
@@ -22,6 +29,7 @@ int main() {
 
         putchar('\n');
     }
+
 
     return 0;
 }
