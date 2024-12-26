f0fa984f1036e712bff7f71fd000a3452c2eedba
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-26 18:16:02 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 09

--- Informatics/2024-2025/Practicums/Group 3/Week 09/Lab/Example 09.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 09/Lab/Example 09.c
@@ -1,79 +1,228 @@
+#include <assert.h>
+
 #include <stdio.h>
 
-#define PI_M    3.14        // Macro
-
-const double PI_V = 3.14;   // Variable
 
 
-void swapFake(int  a, int  b);
-void swapReal(int* a, int* b);
+#define MAX_CAPACITY 100
 
 
-void samples();
+
+void clearStandardInput();
+
+
+
+int             readElem();
+unsigned int    readSize();
+
+
+
+void inputArray(        int array[], unsigned int size);
+void printArray(const   int array[], unsigned int size);
+
+
+
+int* linearSearch(int array[], unsigned int size, int element);
+int* binarySearch(int array[], unsigned int size, int element);
+
+
+
+void intervalSearch(int array[], unsigned int size, int element, int** start, int** final);
+
 
 
 int main() {
-    int x   =   0;
-    int y   =   0;
+    int array[MAX_CAPACITY] = { 0 };
 
-    printf("Enter the number X: "); scanf("%d", &x);
-    printf("Enter the number Y: "); scanf("%d", &y);
+    unsigned int size = readSize();
 
-    swapFake(x, y);
+    inputArray(array, size);
+    printArray(array, size);
 
-    printf("The number X after swap FAKE is: %d\n", x);
-    printf("The number Y after swap FAKE is: %d\n", y);
 
-    swapReal(&x, &y);
+    int* pointer1   =   linearSearch(array, size, 9);
+    int* pointer2   =   binarySearch(array, size, 7);
 
-    printf("The number X after swap REAL is: %d\n", x);
-    printf("The number Y after swap REAL is: %d\n", y);
+    if (pointer1 == NULL) {
+        printf("The element is not in the array!\n");
+    } else {
+        printf("The element is at position: %ld\n", pointer1 - array);
+    }
 
-    samples();
+    if (pointer2 == NULL) {
+        printf("The element is not in the array!\n");
+    } else {
+        printf("The element is at position: %ld\n", pointer2 - array);
+    }
+
+
+    int* start = NULL;
+    int* final = NULL;
+
+    intervalSearch(array, size, 3, &start, &final);
+
+
+    if (start == NULL && final == NULL) {
+        printf("There is not such range!\n");
+    } else {
+        printf("The start pointer value is: %d\n", *start);
+        printf("The final pointer value is: %d\n", *final);
+    }
+
 
     return 0;
 }
 
 
-void swapFake(int  a, int  b) {
+
+void clearStandardInput() {
     int c = 0;
 
-    c   =   a;
-    a   =   b;
-    b   =   c;
-}
-
-void swapReal(int* a, int* b) {
-    int c = 0;
-
-     c  =   *a;
-    *a  =   *b;
-    *b  =    c;
+    while ((c = getchar()) && c != '\n' && c != EOF);
 }
 
 
-void samples() {
-    int a = 3;
-    int b = 9;
-    int c = 7;
 
-    int* p1 = &a;
-    int* p2 = &b;
+int readElem() {
+    int element         =   0;
+    int inputArgument   =   0;
 
-    printf("The value of pointer 1 is: %p\n", p1);
-    printf("The value of pointer 2 is: %p\n", p2);
+    do {
+        inputArgument = scanf("%d", &element);
 
-    printf("Referencing pointer 1 is: %d\n", *p1);
-    printf("Referencing pointer 2 is: %d\n", *p2);
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
 
-    const int e = 1;    // Constant Variable or Constant
-    int const f = 1;    // Constant Variable or Constant
+    return element;
+}
 
-    const int* pointer1 = &a;   pointer1 = &b; // *pointer1 = 10;   (Impossible)
-    int* const pointer2 = &b;   *pointer2 = 1; // pointer2 = &a;    (Impossible)
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
 
-    const int* const pointer3 = &c;
+    do {
+        printf("Enter the size of the array: ");
 
-    // pointer3     = &b; (Impossible)
-    // *pointer3    = 10; (Impossible)
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > MAX_CAPACITY);
+
+    return size;
+}
+
+
+
+void inputArray(int array[], unsigned int size) {
+    assert(size != 0            );
+    assert(size <= MAX_CAPACITY );
+
+    printf("Enter the elements of the array: ");
+
+    for (unsigned int i = 0; i < size; ++i) {
+        array[i] = readElem();
+    }
+}
+
+void printArray(const int array[], unsigned int size) {
+    assert(size != 0            );
+    assert(size <= MAX_CAPACITY );
+
+    const int* const startPointer = array       ;
+    const int* const finalPointer = array + size;
+
+    printf("The elements of the array are: ");
+
+    for (const int* current = startPointer; current < finalPointer; ++current) {
+        printf("%d ", *current);
+    }
+
+    putchar('\n');
+}
+
+
+
+int* linearSearch(int array[], unsigned int size, int element) {
+    assert(size != 0            );
+    assert(size <= MAX_CAPACITY );
+    
+    int* pointerCurrent = array;
+
+    while (pointerCurrent <= array + size) {
+        if (*pointerCurrent == element) {
+            return pointerCurrent;
+        }
+
+        pointerCurrent++;
+    }
+
+    return NULL;
+}
+
+int* binarySearch(int array[], unsigned int size, int element) {
+    assert(size != 0            );
+    assert(size <= MAX_CAPACITY );
+
+    int* pointerLeft    =   array       ;
+    int* pointerRight   =   array + size;
+
+    while (pointerLeft < pointerRight) {
+        int* pointerMiddle = pointerLeft + (pointerRight - pointerLeft) / 2;
+
+        if (*pointerMiddle == element) {
+            return pointerMiddle;
+        }
+
+        if (*pointerMiddle > element) {
+            pointerRight = pointerMiddle;
+        } else {
+            pointerLeft = pointerMiddle + 1;
+        }
+    }
+
+    return NULL;
+}
+
+
+
+void intervalSearch(int array[], unsigned int size, int element, int** start, int** final) {
+    assert(size != 0            );
+    assert(size <= MAX_CAPACITY );
+
+    int* pointer1   =   array       ;
+    int* pointer2   =   array + size;
+
+    while (pointer1 < pointer2) {
+        int* middle = pointer1 + (pointer2 - pointer1) / 2;
+
+        if (*middle >= element) {
+            pointer2 = middle;
+        } else {
+            pointer1 = middle + 1;
+        }
+    }
+
+    if (*pointer1 != element) {
+        return;
+    }
+
+    *start  =   pointer1        ;
+
+    pointer2 = array + size;
+
+    while (pointer1 < pointer2) {
+        int* middle = pointer1 + (pointer2 - pointer1) / 2;
+
+        if (*middle > element) {
+            pointer2 = middle;
+        } else {
+            pointer1 = middle + 1;
+        }
+    }
+
+    *final  =   pointer1        ;
 }
