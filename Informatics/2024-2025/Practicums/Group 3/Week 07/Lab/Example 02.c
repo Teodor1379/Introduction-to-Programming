8df3e5e54b0044e7f7e974681bb1cd580cb69a8e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-26 15:25:06 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 07

--- Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 02.c
+++ Informatics/2024-2025/Practicums/Group 3/Week 07/Lab/Example 02.c
@@ -7,6 +7,20 @@ int main() {
     int array2[]  = { 1, 2, 3 };
 
 
+    unsigned int size1 = sizeof(array1) / sizeof(int);
+    unsigned int size2 = sizeof(array2) / sizeof(int);
+
+
+    printf("The start element of the array 1 is: %d\n", array1[        0]);
+    printf("The final element of the array 1 is: %d\n", array1[size1 - 1]);
+
+    printf("The start element of the array 2 is: %d\n", array2[        0]);
+    printf("The final element of the array 2 is: %d\n", array2[size2 - 1]);
+
+    printf("Invalid Value 1 is: %d\n",  array1[   -1]);
+    printf("Invalid Value 2 is: %d\n",  array2[size2]);
+
+
     for (unsigned int i = 0; i < 9; ++i) {
         printf("%d ", array1[i]);
     }
