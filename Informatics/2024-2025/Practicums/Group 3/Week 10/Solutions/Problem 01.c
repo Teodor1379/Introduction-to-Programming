eb66015d3467a3cbb135198db1d8bc955e552e2e
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-30 12:42:06 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 10

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 10/Solutions/Problem 01.c
@@ -0,0 +1,158 @@
+#include <assert.h>
+
+#include <limits.h>
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+void clearStandardInput();
+
+
+
+int             readElem();
+unsigned int    readSize();
+
+
+
+int* buildArray(                    unsigned int* size  );
+int* cloneArray(const   int* array, unsigned int  size  );
+
+void inputArray(        int* array, unsigned int  size  );
+void printArray(const   int* array, unsigned int  size  );
+
+
+
+int main() {
+    unsigned int size = readSize();
+
+
+    int* array = buildArray(&size);
+
+    if (array == NULL) {
+        puts("Allocating Array Error!");
+
+        return 1;
+    }
+
+
+    int* clone = cloneArray(array, size);
+
+    if (clone == NULL) {
+        puts("Allocating Array Error!");
+
+        free(array);
+
+        return 1;
+    }
+
+
+    printArray(clone, size);
+
+
+    free(array);    array   =   NULL;
+    free(clone);    clone   =   NULL;
+
+
+    return 0;
+}
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+int readElem() {
+    int elem            =   0;
+    int inputArgument   =   0;
+
+    do {
+        inputArgument = scanf("%d", &elem);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1);
+
+    return elem;
+}
+
+unsigned int readSize() {
+    unsigned int size   =   0;
+    int inputArgument   =   0;
+
+    do {
+        printf("Enter the size of the array: ");
+
+        inputArgument = scanf("%u", &size);
+
+        if (inputArgument != 1) {
+            clearStandardInput();
+        }
+    } while (inputArgument != 1 || size == 0 || size > UINT_MAX);
+
+    return size;
+}
+
+
+
+int* buildArray(unsigned int* size) {
+    assert(*size != 0);
+
+    int* array = malloc(*size * sizeof(int));
+
+    if (array != NULL) {
+        inputArray(array, *size);
+        printArray(array, *size);
+    }
+
+    return array;
+}
+
+int* cloneArray(const int* array, unsigned int size) {
+    assert(array    !=  NULL    );
+    assert(size     !=  0       );
+
+    int* clone = malloc(size * sizeof(int));
+
+    if (clone != NULL) {
+        for (unsigned int i = 0; i < size; ++i) {
+            clone[i] = array[i];
+        }
+    }
+
+    return clone;
+}
+
+
+
+void inputArray(int* array, unsigned int size) {
+    assert(array    != NULL );
+    assert(size     != 0    );
+
+    printf("Enter the elements of the array: ");
+
+    for (unsigned int i = 0; i < size; ++i) {
+        array[i] = readElem();
+    }
+    
+}
+
+void printArray(const int* array, unsigned int size) {
+    assert(array    !=  NULL    );
+    assert(size     !=  0       );
+
+    printf("The elements of the array are: ");
+
+    for (unsigned int i = 0; i < size; ++i) {
+        printf("%d ", array[i]);
+    }
+
+    putchar('\n');
+}
