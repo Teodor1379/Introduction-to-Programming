ae83b29594e36018f2fd70ac100b9524c35096a2
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-30 17:24:25 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 11

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 11/Lab/Example 03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+int main() {
+    const char* string1 = "Teo";
+    const char* string2 = "Jess";
+    const char* string3 = "Love";
+
+
+    printf("The length of String 1 is: %ld\n", strlen(string1));
+    printf("The length of String 2 is: %ld\n", strlen(string2));
+    printf("The length of String 3 is: %ld\n", strlen(string3));
+
+
+    return 0;
+}
