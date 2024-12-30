e7ca5bb25daa33645189550ae6ef2cd007c45a64
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-30 17:35:04 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 11

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 11/Lab/Example 05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+int main() {
+    printf("The result of the compare is: %d\n",    strcmp("Teo_1", "Teo_2"));
+    printf("The result of the compare is: %d\n",    strcmp("Teo_2", "Teo_1"));
+    printf("The result of the compare is: %d\n",    strcmp("Jessi", "Jessi"));
+
+
+    return 0;
+}
