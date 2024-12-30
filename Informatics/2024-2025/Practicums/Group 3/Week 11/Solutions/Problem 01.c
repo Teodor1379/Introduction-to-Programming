4ce4f1b334921d1dc4844eab36d67e3c0397d496
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-12-30 22:11:00 +0200

    Commit: Informatics - 2024-2025 - Practicums - Group 3 - Week 11

---
+++ Informatics/2024-2025/Practicums/Group 3/Week 11/Solutions/Problem 01.c
@@ -0,0 +1,106 @@
+#include <assert.h>
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+#define MAX 1024
+
+
+
+void clearStandardInput();
+
+
+
+char    readCharacter();
+
+
+
+char*   buildString(                );
+void    clearString(char* string    );
+
+
+
+unsigned int countOccurences(const char* string, char character);
+
+
+
+int main() {
+    char* string = buildString();
+
+    if (string == NULL) {
+        puts("Allocating Memory... ERROR!");
+
+        return 1;
+    }
+
+    
+    char character = readCharacter();
+
+
+    printf("The occurences are: %u\n",  countOccurences(string, character));
+
+
+    clearString(string);
+
+
+    return 0;
+}
+
+
+
+void clearStandardInput() {
+    int c = 0;
+
+    while ((c = getchar()) && c != '\n' && c != EOF);
+}
+
+
+
+char readCharacter() {
+    char character = 0;
+
+    printf("Enter the character: ");
+
+    character = getchar();
+
+    return character;
+}
+
+
+
+char* buildString() {
+    printf("Enter the string: ");
+
+    char* string = (char*)(malloc(MAX * sizeof(char)));
+
+    if (string != NULL) {
+        fgets(string, MAX, stdin);
+    } 
+
+    return string;
+}
+
+void clearString(char* string) {
+    assert(string   != NULL );
+
+    free(string);
+
+    string = NULL;
+}
+
+
+
+unsigned int countOccurences(const char* string, char character) {
+    unsigned int result = 0;
+
+    for (unsigned int i = 0; string[i] != '\0'; ++i) {
+        if (string[i] == character) {
+            result = result + 1;
+        }
+    }
+
+    return result;
+}
